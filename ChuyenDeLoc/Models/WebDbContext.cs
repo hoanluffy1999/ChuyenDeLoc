fc483edfede0d926753ef98e559906202ac73bf9
hoanv4 <hoannv@edmicro.vn>
2021-10-05 22:57:03 +0700

    Add project files.

---
+++ ChuyenDeLoc/Models/WebDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChuyenDeLoc.Models
+{
+    public  class WebDbContext
+    {
+        public QLCayCanhEntities Instanse { get; }
+        public WebDbContext()
+        {
+            if(Instanse == null)
+            {
+               Instanse = new QLCayCanhEntities();
+            } 
+        }
+    }
+}
