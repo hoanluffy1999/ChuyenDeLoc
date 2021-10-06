b8a5ca2bff6bfd44918116d4dd84533e01418afb
hoanv4 <hoannv@edmicro.vn>
2021-10-07 00:13:22 +0700

    git commit

--- ChuyenDeLoc/Controllers/AccountController.cs
+++ ChuyenDeLoc/Controllers/AccountController.cs
@@ -5,17 +5,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.Mvc;
 
 namespace ChuyenDeLoc.Controllers
 {
-    public class AccountController
+    public class NhanVienController : Controller
     {
         private readonly QLCayCanhEntities db;
-        public AccountController()
+        public NhanVienController()
         {
             WebDbContext webDbContext = new WebDbContext();
             db = webDbContext.GetDBContext();
         }
-       
+        public ActionResult Index()
+        {
+            ViewBag.title = "Danh sách tài khoản";
+            return View();
+        }
+        [HttpGet]
+        public ActionResult GetList(string name)
+        {
+            var data = db.NhanViens.Where(x =>  (x.TenDangNhap.ToLower().Contains(name.ToLower()) || string.IsNullOrEmpty(name))).ToList();
+            return PartialView(data);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult Create(NhanVien inputModel)
+        {
+           
+            db.NhanViens.Add(inputModel);
+            db.SaveChanges();
+            return Json(new { result = true }); ;
+        }
+        [HttpGet]
+        public ActionResult Update(int id)
+        {
+            var entity = db.NhanViens.Find(id);
+            return PartialView(entity);
+        }
+        [HttpPost]
+        public ActionResult Update(NhanVien inputModel)
+        {
+
+
+            var entity = db.NhanViens.Find(inputModel.Ma);
+            {
+                return Json(new { result = false });
+            }
+           
+            db.Entry(entity).CurrentValues.SetValues(inputModel);
+            db.SaveChanges();
+            return Json(new { result = true });
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+
+
+            var entity = db.NhanViens.Find(id);
+
+            if (entity == null)
+            {
+                return Json(new { result = false });
+            }
+           
+            db.Entry(entity).CurrentValues.SetValues(entity);
+            db.SaveChanges();
+            return Json(new { result = true }); ;
+        }
     }
 }
